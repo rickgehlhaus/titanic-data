a2de732ae960644577d8b3824604e773e83469bc
Rick Gehlhaus <rickgehlhaus@gmail.com>
2016-05-05 10:56:26 -0700

    Update titan_wrangle.R
    final edit

--- titan_wrangle.R
+++ titan_wrangle.R
@@ -1,13 +1,17 @@
-library(plyr)
 library(dplyr)
-library(tidyr)
 df <- read.csv("titanic_original.csv")  #importing csv file
 df <- tbl_df(df)
 
+# initial setup complete-----------------------------------------------------------
 
 df$embarked <- sub("^$", "S", df$embarked)  #adding "S" to blank values in embarked col.
 
-df$age[is.na(df$age)] <- mean(df$age, na.rm = TRUE)
+df$age[is.na(df$age)] <- mean(df$age, na.rm = TRUE)   #add mean age to na values in age col.
 
+df$boat <- sub("^$", "None", df$boat)   #filled empty rows with "None" in boat col.
 
-View(df)
+df$has_cabin_number <- ifelse(df$cabin == "", 0, 1) #
+
+# clean up complete -------------------------------------------------------
+
+write.csv(df, "titanic_clean.csv")   #export cleaned up data
