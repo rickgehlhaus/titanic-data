7914ec44a76fa0d006f72ff6d39c89329ddc432a
Rick Gehlhaus <rickgehlhaus@gmail.com>
2016-05-05 09:34:56 -0700

    Update titan_wrangle.R

--- titan_wrangle.R
+++ titan_wrangle.R
@@ -7,8 +7,7 @@ df <- tbl_df(df)
 
 df$embarked <- sub("^$", "S", df$embarked)  #adding "S" to blank values in embarked col.
 
-age_mean <- mean(df$age, na.rm = TRUE)
-df$age[is.na(df$age)] <- age_mean
+df$age[is.na(df$age)] <- mean(df$age, na.rm = TRUE)
 
 
 View(df)
